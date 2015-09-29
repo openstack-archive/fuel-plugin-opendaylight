916e1c4fdd298906828f9f4bb13ab40e7eacd44c
Michal Skalski <mskalski@mirantis.com>
2015-09-29 15:12:31 +0200

    Fix MTU for vxlan segmentation
    We need to decrease MTU for VMs when vxlan is used.
    
    Change-Id: I117384313d03e64b4b70ca42d963628bf509aaf2
    (cherry picked from commit 5ddc49a7bea6aabf83eaa3c4298860fa2e4f66bd)

--- deployment_scripts/puppet/manifests/ml2-configuration.pp
+++ deployment_scripts/puppet/manifests/ml2-configuration.pp
@@ -5,6 +5,10 @@ $port = $opendaylight::rest_api_port
 $vni_start = $opendaylight::odl_settings['vni_range_start']
 $vni_end = $opendaylight::odl_settings['vni_range_end']
 
+$neutron_settings = hiera('quantum_settings')
+$network_scheme = hiera('network_scheme', {})
+prepare_network_config($network_scheme)
+
 neutron_plugin_ml2 {
   'ml2/mechanism_drivers':      value => 'opendaylight';
   'ml2_odl/password':           value => 'admin';
@@ -12,9 +16,27 @@ neutron_plugin_ml2 {
   'ml2_odl/url':                value => "http://${address}:${port}/controller/nb/v2/neutron";
 }
 
-if $opendaylight::odl_settings['use_vxlan'] {
-  neutron_plugin_ml2 {
-    'ml2/tenant_network_types':   value => 'vxlan';
-    'ml2_type_vxlan/vni_ranges':   value => "${vni_start}:${vni_end}";
+$segmentation_type = $neutron_settings['L2']['segmentation_type']
+if $segmentation_type != 'vlan' {
+  # MTU need to be static because ODL ignore MTU value from neturon
+  # and always create tap interfaces for VMs with MTU 1500
+  if $opendaylight::odl_settings['use_vxlan'] {
+    neutron_plugin_ml2 {
+      'ml2/tenant_network_types':   value => 'vxlan';
+      'ml2_type_vxlan/vni_ranges':   value => "${vni_start}:${vni_end}";
+    }
+    $mtu = 1450
+  } else {
+    $mtu = 1458
+  }
+
+  neutron_config {
+    'DEFAULT/network_device_mtu':  value => $mtu;
+  }
+
+  file { '/etc/neutron/dnsmasq-neutron.conf':
+      owner   => 'root',
+      group   => 'root',
+      content => template('openstack/neutron/dnsmasq-neutron.conf.erb'),
   }
 }
