5d08186fffbba2e14db7119892354349b51efd2d
Ferenc Cserepkei <ferenc.cserepkei@ericsson.com>
2015-10-18 18:13:45 +0200

    Add SFC feature to an ODL based openstack deployment
    This commit adds Service Function Chaining(SFC) basic installation opportunity
    to a Fuel orchestrated openstack cluster. SFC installation is integrated into
    fuel-plugin-opendaylight, since it covers orchestration tasks only and
    the used SFC is closely tied to the OpenDayLight installation used.
    The Deployment has 2 main tasks:
    - install the appropriate karaf features into OpenDaylight
    - do some OpenStack configurations for ease the use of SFC
    
    Change-Id: I837183f33f05db7c9c082fe1370837c0663e1d14
    Signed-off-by: Ferenc Cserepkei <ferenc.cserepkei@ericsson.com>

---
+++ deployment_scripts/sfc-defaults.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /root/openrc
+
+#allow ssh login
+nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
+#allow ping
+nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
+#vnf template: 512Mbytes RAM, 3Gbytes disk, 1 CPU
+nova flavor-create m1.vnf 6 512 3 1
