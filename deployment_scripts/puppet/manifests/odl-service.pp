7d81aab65c989344565ec4e03897fbce0748d245
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-01-27 15:11:27 +0100

    Add possiblity to use external ODL
    It is not always wanted that ODL is running on the controller.
    In this cases a pre setup ODL might be conntected to
    the cloud. This commit enables the plugin to use this
    already deployed ODL controller.
    
    Change-Id: If2c9430045e2b61f1fe98e0806bbaf9f752a213d

--- deployment_scripts/puppet/manifests/odl-service.pp
+++ deployment_scripts/puppet/manifests/odl-service.pp
@@ -1,1 +1,4 @@
-class { 'opendaylight::service':}
+$odl = hiera('opendaylight')
+unless $odl['use_external_odl']{
+    class { 'opendaylight::service':}
+}
--- deployment_scripts/puppet/modules/opendaylight/manifests/init.pp
+++ deployment_scripts/puppet/modules/opendaylight/manifests/init.pp
@@ -3,9 +3,15 @@ class opendaylight {
   $nodes_hash = hiera('nodes')
   $odl_controller_hash = filter_nodes($nodes_hash,'role','opendaylight')
   $node = filter_nodes($nodes_hash,'name',$::hostname)
-
+  $odl = hiera('opendaylight')
+  if $odl['use_external_odl'] {
+    $manager_ip_address = $odl['rest_api_ip']
+    $odl_management_vip = $odl['rest_api_ip']
+  } else {
+    $manager_ip_address = $odl_controller_hash[0]['internal_address']
+    $odl_management_vip = hiera('management_vip')
+  }
   $rest_api_port = $odl_settings['rest_api_port']
-  $manager_ip_address = $odl_controller_hash[0]['internal_address']
   $node_private_address = $node[0]['private_address']
   $node_internal_address = $node[0]['internal_address']
 }
