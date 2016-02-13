6f68010accf719d904657db3f397ba6a9b6676d5
Michal Skalski <mskalski@mirantis.com>
2016-02-13 01:48:00 +0100

    Provide possibility to build plugin with openjdk-8
    Daniel Smith discover large resource consumption when SFC features are
    enabled and openjdk-7 is used. Following his advice we want to add
    option to use openjdk-8. Becuase this version is not offically available
    in ubuntu 14.04, it needs to be included inside plugin. This can be achieved
    by setting USE_JAVA8=true environment variable during plugin compilation:
    
      USE_JAVA8=true fpb --debug --build fuel-plugin-opendaylight
    
    Change-Id: If1318c766b31ff9749bd3d75f8ee3c5ca6f89ffc
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- deployment_scripts/puppet/modules/opendaylight/manifests/install.pp
+++ deployment_scripts/puppet/modules/opendaylight/manifests/install.pp
@@ -15,20 +15,8 @@ class opendaylight::install (
     $manage_l3_traffic = 'no'
   }
 
-
-  $java_package = $operatingsystem ? {
-    'CentOS' => 'java-1.7.0-openjdk',
-    'Ubuntu' => 'openjdk-7-jre-headless',
-  }
-
-  package { 'java-jre':
-    ensure => installed,
-    name   => $java_package,
-  }
-
   package { 'opendaylight':
     ensure  => installed,
-    require => Package['java-jre'],
   }
 
   package {'opnfv-quagga':
