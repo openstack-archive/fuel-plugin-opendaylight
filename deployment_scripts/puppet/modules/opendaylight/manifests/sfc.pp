e8a0cd26545413ce8221af06406496b874b5dc1c
Manuel Buil <manuel.buil@ericsson.com>
2016-05-17 16:39:58 +0200

    App coexistence configuration for Netvirt and SFC  added, so that it gets configured before the switches are connected to ODL
    Change-Id: Id59c2bf062bdaee3508f25c5461f839e7ae84758
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

---
+++ deployment_scripts/puppet/modules/opendaylight/manifests/sfc.pp
@@ -0,0 +1,27 @@
+class opendaylight::sfc {
+    include opendaylight
+    $management_vip = hiera('management_vip')
+    $odl = hiera('opendaylight')
+    $rest_port = $odl['rest_api_port']
+    $user = $odl['metadata']['default_credentials']['user']
+    $password = $odl['metadata']['default_credentials']['password']
+
+    if $odl['enable_sfc'] {
+        if $odl['sfc_class']=='ncr' {
+            if roles_include(['primary-controller']) {
+                exec { 'odl_netvirt_coexistence':
+                command   => "/usr/bin/curl -i -u ${user}:${password} \
+-H 'Content-type: application/json' \
+-X PUT -d '{\"netvirt-providers-config\":{\"table-offset\":\"1\"}}' \
+http://${management_vip}:${rest_port}/restconf/config/netvirt-providers-config:netvirt-providers-config",
+                }
+                exec { 'odl_sfc_coexistence':
+                command   => "/usr/bin/curl -i -u ${user}:${password} \
+-H 'Content-type: application/json' -X PUT -d \
+'{\"sfc-of-renderer-config\":{\"sfc-of-table-offset\":\"150\",\"sfc-of-app-egress-table-offset\":\"11\"}}' \
+http://${management_vip}:${rest_port}/restconf/config/sfc-of-renderer:sfc-of-renderer-config",
+                }
+            }
+        }
+    }
+}
