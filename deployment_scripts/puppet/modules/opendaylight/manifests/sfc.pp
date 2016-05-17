a9ff21aa1cc18896038710ff605612f9cb106f4f
Manuel Buil <manuel.buil@ericsson.com>
2016-05-17 16:39:58 +0200

    App coexistence configuration for Netvirt and SFC
    Rest API config so that Netvirt and SFC are configured
    before the switches from computes are connected to ODL
    
    Change-Id: Id59c2bf062bdaee3508f25c5461f839e7ae84758
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

---
+++ deployment_scripts/puppet/modules/opendaylight/manifests/sfc.pp
@@ -0,0 +1,23 @@
+class opendaylight::sfc {
+    include opendaylight
+    $management_vip = hiera('management_vip')
+    $odl = hiera('opendaylight')
+    $rest_port = $odl['rest_api_port']
+
+    if $odl['enable_sfc'] {
+        if $odl['sfc_class']=='ncr' {
+            exec { 'odl_netvirt_coexistence':
+            command   => "/usr/bin/curl -i -u admin:admin \
+-H 'Content-type: application/json' \
+-X PUT -d '{\"netvirt-providers-config\":{\"table-offset\":\"1\"}}' \
+http://${management_vip}:${rest_port}/restconf/config/netvirt-providers-config:netvirt-providers-config",
+                }
+            exec { 'odl_sfc_coexistence':
+            command   => "/usr/bin/curl -i -u admin:admin \
+-H 'Content-type: application/json' -X PUT -d \
+'{\"sfc-of-renderer-config\":{\"sfc-of-table-offset\":\"150\",\"sfc-of-app-egress-table-offset\":\"11\"}}' \
+http://${management_vip}:${rest_port}/restconf/config/sfc-of-renderer:sfc-of-renderer-config",
+            }
+        }
+    }
+}
