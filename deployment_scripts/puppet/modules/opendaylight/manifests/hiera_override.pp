746def7fd9f0a3a02beb5c3c8607678f3ef7c7d7
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-06-14 21:12:34 +0300

    Fuel 10 uplift
    - generate override actions
    - clean up outdated manifests
    - align function calls
    
    Change-Id: Ie1bf8f2360f72f9de4d3f63c0a6b397f3e94909d

--- deployment_scripts/puppet/modules/opendaylight/manifests/hiera_override.pp
+++ deployment_scripts/puppet/modules/opendaylight/manifests/hiera_override.pp
@@ -1,5 +1,17 @@
 class opendaylight::hiera_override {
+  include opendaylight
   $override_file = '/etc/hiera/plugins/opendaylight.yaml'
-  $roles = hiera('roles')
-  odl_hiera_overrides($override_file, $roles)
+
+  # override network scheme
+  $orig_network_scheme = hiera_hash('network_scheme')
+  $network_scheme = odl_network_scheme($opendaylight::odl_settings['enable_bgpvpn'], $orig_network_scheme)
+
+  odl_hiera_overrides(
+    $override_file,
+    $opendaylight::odl_settings,
+    hiera('neutron_config'),
+    hiera('neutron_advanced_configuration'),
+    $network_scheme,
+    hiera('management_vip')
+  )
 }
