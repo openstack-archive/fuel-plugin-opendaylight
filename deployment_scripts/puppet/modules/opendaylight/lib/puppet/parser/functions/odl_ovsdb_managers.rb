5d80efce6d0b1d242489ec2897cf20f51dd5fcb1
Michal Skalski <mskalski@mirantis.com>
2016-07-05 16:47:55 +0200

    Generate proper list of ovsdb managers
    Currently we not iterate through array returned by flat_map method and
    list of ovsdb managers is in the form:
      tcp:["192.168.0.3", "192.168.0.5"]:6640
    Use flatten.map which produce:
      tcp:192.168.0.3:6640 tcp:192.168.0.5:6640
    
    Change-Id: Ic09fea905cba3e532706d4a829b9670284a2ecbc
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- deployment_scripts/puppet/modules/opendaylight/lib/puppet/parser/functions/odl_ovsdb_managers.rb
+++ deployment_scripts/puppet/modules/opendaylight/lib/puppet/parser/functions/odl_ovsdb_managers.rb
@@ -1,8 +1,12 @@
 module Puppet::Parser::Functions
-  newfunction(:odl_ovsdb_managers, :arity => 1, :type => :rvalue) do |args|
-
-    managers = args.flat_map { |ip| "tcp:#{ip}:6640" }
+  newfunction(:odl_ovsdb_managers, :arity => 1, :type => :rvalue, :doc => <<-'EOS'
+    @desc Creates list of ovsdb managers used in ovs-vsctl set-manager command
+    @input ['192.168.1.7', ....]
+    @return 'tcp:192.168.1.7:6640 ....'
+    @example odl_ovsdb_managers($ovsdb_mng)
+    EOS
+  ) do |args|
+    managers = args.flatten.map { |ip| "tcp:#{ip}:6640" }
     managers.join(' ')
-
   end
 end
