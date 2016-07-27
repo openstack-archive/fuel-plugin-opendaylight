da08a2b5e707490f3434abff5b886bfd18d23d60
Manuel Buil <manuel.buil@ericsson.com>
2016-07-27 11:25:03 +0200

    Modified the new SFC features for ODL Boron
    Enabling SFC implies ODL Boron usage.
    
    Change-Id: Ia5e6339c2726822baa26e733ca8b8c9f79243446
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- deployment_scripts/puppet/modules/opendaylight/manifests/init.pp
+++ deployment_scripts/puppet/modules/opendaylight/manifests/init.pp
@@ -3,7 +3,6 @@ class opendaylight {
   $network_metadata = hiera_hash('network_metadata')
   $node_uid = hiera('uid')
   $rest_api_port = $odl_settings['rest_api_port']
-  $package_name = $odl_settings['package_name']
   $jetty_port = $odl_settings['metadata']['jetty_port']
   $odl_nodes_hash = get_nodes_hash_by_roles($network_metadata, ['opendaylight'])
   $odl_mgmt_ips_hash = get_node_to_ipaddr_map_by_network_role($odl_nodes_hash, 'management')
