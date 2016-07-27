2e049ecc4ec1b8c3adb0f91b7f9aff89dd304a75
Manuel Buil <manuel.buil@ericsson.com>
2016-07-27 11:25:03 +0200

    Modified the new SFC features for ODL Boron
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>
    
    Change-Id: Ic7d7d4f933d317152c749ccac1ed51024f18a7f4
    ethfci: Selecetd ODL version dependent boot feature list
    Signed-off-by: Ferenc Cserepkei <ferenc.cserepkei@ericsson.com>

--- deployment_scripts/puppet/modules/opendaylight/lib/puppet/parser/functions/odl_karaf_features.rb
+++ deployment_scripts/puppet/modules/opendaylight/lib/puppet/parser/functions/odl_karaf_features.rb
@@ -7,7 +7,8 @@ module Puppet::Parser::Functions
     enabled_features << features_set['default']
     enabled_features << features_set['odl-default']
     enabled_features << features_set['ovs'] if not odl_settings['enable_bgpvpn']
-    enabled_features << features_set['sfc'] if odl_settings['enable_sfc']
+    enabled_features << features_set['sfc'] if odl_settings['enable_sfc'] and (odl_settings['package_name'] == 'opendaylight')
+    enabled_features << features_set['boron-sfc'] if odl_settings['enable_sfc'] and (odl_settings['package_name'] == 'opendaylight-boron')
     enabled_features << features_set[odl_settings['sfc_class']] if odl_settings['enable_sfc']
     enabled_features << features_set['vpn'] if odl_settings['enable_bgpvpn']
 
