43b990aac9f4a799eee58b2a775b8f0de204b2a4
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-08-23 15:18:23 +0200

    Uplift to Boron
    Change-Id: Icdb962dac83d0a68b25914285a0e7d8987daefdc

--- deployment_scripts/puppet/modules/opendaylight/lib/puppet/parser/functions/odl_network_scheme.rb
+++ deployment_scripts/puppet/modules/opendaylight/lib/puppet/parser/functions/odl_network_scheme.rb
@@ -10,12 +10,7 @@ module Puppet::Parser::Functions
     odl = function_hiera(['opendaylight'])
     network_scheme = function_hiera(['network_scheme'])
 
-    if odl['enable_bgpvpn']
-      # If bgpvpn extensions are enabled br-ex is not needed
-      delete_bridges = ['br-prv', 'br-floating']
-    else
-      delete_bridges = ['br-prv']
-    end
+    delete_bridges = ['br-prv']
 
     debug "ODL network before transformation: #{network_scheme}"
 
@@ -25,46 +20,33 @@ module Puppet::Parser::Functions
     transformations.delete_if { |action| action['action'] == 'add-patch' and not (action['bridges'] & delete_bridges).empty? }
     transformations.delete_if { |action| action['action'] == 'add-port' and delete_bridges.include?(action['bridge']) }
 
-    if not odl['enable_bgpvpn']
-      debug "Changing network_scheme for the non bgpvpn case."
-      # Modify only once
-      if not endpoints.has_key? 'br-ex-lnx'
-        transformations.each { |action| action['name'] = 'br-ex-lnx' if (action['action'] == 'add-br' and action['name'] == 'br-ex') }
-        transformations.each { |action| action['bridge'] = 'br-ex-lnx' if (action['action'] == 'add-port' and action['bridge'] == 'br-ex') }
-        transformations.each { |action| action['bridge'] = 'br-ex-lnx' if (action['action'] == 'add-bond' and action['bridge'] == 'br-ex') }
-      end
+    # Modify only once
+    if not endpoints.has_key? 'br-ex-lnx'
+      transformations.each { |action| action['name'] = 'br-ex-lnx' if (action['action'] == 'add-br' and action['name'] == 'br-ex') }
+      transformations.each { |action| action['bridge'] = 'br-ex-lnx' if (action['action'] == 'add-port' and action['bridge'] == 'br-ex') }
+      transformations.each { |action| action['bridge'] = 'br-ex-lnx' if (action['action'] == 'add-bond' and action['bridge'] == 'br-ex') }
+    end
 
-      transformations.each { |action| action['name'] = 'br-ex' if (action['action'] == 'add-br' and action['name'] == 'br-floating') }
-      transformations.each { |action| action['bridge'] = 'br-ex' if (action['action'] == 'add-port' and action['bridge'] == 'br-floating') }
+    transformations.each { |action| action['name'] = 'br-ex' if (action['action'] == 'add-br' and action['name'] == 'br-floating') }
+    transformations.each { |action| action['bridge'] = 'br-ex' if (action['action'] == 'add-port' and action['bridge'] == 'br-floating') }
 
-      transformations.each { |action| action['bridges'] = ['br-ex', 'br-ex-lnx'] if (action['action'] == 'add-patch' and action['bridges'] == ['br-floating', 'br-ex']) }
+    transformations.each { |action| action['bridges'] = ['br-ex', 'br-ex-lnx'] if (action['action'] == 'add-patch' and action['bridges'] == ['br-floating', 'br-ex']) }
 
-      roles = network_scheme['roles']
-      roles.each { |role,bridge| roles[role] = 'br-ex-lnx' if bridge == 'br-ex' }
-      roles['neutron/private'] = 'br-aux' if roles.has_key?('neutron/private')
-      roles['neutron/floating'] = 'br-ex' if roles.has_key?('neutron/floating')
+    roles = network_scheme['roles']
+    roles.each { |role,bridge| roles[role] = 'br-ex-lnx' if bridge == 'br-ex' }
+    roles['neutron/private'] = 'br-aux' if roles.has_key?('neutron/private')
+    roles['neutron/floating'] = 'br-ex' if roles.has_key?('neutron/floating')
 
-      if endpoints.has_key? 'br-ex' and not endpoints.has_key? 'br-ex-lnx'
-        endpoints['br-ex-lnx'] = endpoints.delete 'br-ex'
-      end
+    if endpoints.has_key? 'br-ex' and not endpoints.has_key? 'br-ex-lnx'
+      endpoints['br-ex-lnx'] = endpoints.delete 'br-ex'
+    end
 
-      if endpoints.has_key? 'br-floating'
-         endpoints['br-ex'] = endpoints.delete 'br-floating'
-      end
+    if endpoints.has_key? 'br-floating'
+       endpoints['br-ex'] = endpoints.delete 'br-floating'
+    end
 
-      if endpoints.has_key? 'br-prv'
-         endpoints['br-aux'] = endpoints.delete 'br-prv'
-      end
-    else
-      debug "Changing network_scheme for the bgpvpn case"
-      roles = network_scheme['roles']
-      roles['neutron/floating'] = 'None' if roles.has_key?('neutron/floating')
-      if endpoints.has_key? 'br-floating'
-         endpoints.delete 'br-floating'
-      end
-      if endpoints.has_key? 'br-prv'
-         endpoints.delete 'br-prv'
-      end
+    if endpoints.has_key? 'br-prv'
+       endpoints['br-aux'] = endpoints.delete 'br-prv'
     end
     debug "ODL network after transformation: #{network_scheme}"
     network_scheme
