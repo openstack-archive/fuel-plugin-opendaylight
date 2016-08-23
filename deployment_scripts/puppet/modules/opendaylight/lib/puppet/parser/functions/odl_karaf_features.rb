0976f6a782fe64b26bf0e729dea32f7e32be4faa
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-08-23 15:18:23 +0200

    Uplift BGPVPN to Boron-SR1
    Change-Id: Icdb962dac83d0a68b25914285a0e7d8987daefdc

--- deployment_scripts/puppet/modules/opendaylight/lib/puppet/parser/functions/odl_karaf_features.rb
+++ deployment_scripts/puppet/modules/opendaylight/lib/puppet/parser/functions/odl_karaf_features.rb
@@ -6,10 +6,9 @@ module Puppet::Parser::Functions
     enabled_features = []
     enabled_features << features_set['default']
     enabled_features << features_set['odl-default']
-    enabled_features << features_set['ovs'] if not odl_settings['enable_bgpvpn']
+    enabled_features << features_set['ovs']
     enabled_features << features_set['sfc'] if odl_settings['enable_sfc']
     enabled_features << features_set[odl_settings['sfc_class']] if odl_settings['enable_sfc']
-    enabled_features << features_set['vpn'] if odl_settings['enable_bgpvpn']
 
     enabled_features.join(',')
   end
